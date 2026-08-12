359cb85210cd0efac6f80700983bea09624ec663
sayani-code <sayanisaha0806@gmail.com>
2026-08-12 20:31:45 +0530

    Add file categorization

--- main.py
+++ main.py
@@ -2,9 +2,29 @@
 import os
 import shutil
 
+file_categories = {
 
-file_names=os.listdir(input("Enter folder path: "))
-print(file_names)
+   "Images":[".jpg" ,".png" ,".jpeg"],
+   "Documents":[".pdf" ,".txt" ,".docx"],
+   "Music":[".mp3",".wav"],
+   "Videos":[".mp4",".mkv"],
+   "Python":[".py"],
+
+}
+
+path=input("Enter folder path: ")
+file_names=os.listdir(path)
+
 for file_name in file_names:
-    name,extention=os.path.splitext(file_name)
-    print(name,"     ",extention)
+
+    name,extension=os.path.splitext(file_name)
+    category_found = False
+
+    for category in file_categories:
+            if extension in file_categories[category]:
+                category_found = True
+            
+
+    if not category_found:
+        category = "Others"
+        print(file_name, "→", category)
